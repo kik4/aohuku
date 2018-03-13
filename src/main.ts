075124574bd4f1fd8160e2f5683335797a3510f2
kik4 <7163720+kik4@users.noreply.github.com>
2018-03-14 00:27:23 +0900

    fetch url and update configs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import vue from 'vue';
+import Vue from 'vue';
 
 // reset contents
 document.head.innerHTML = `
@@ -12,7 +12,11 @@ document.body.innerHTML = `
 </div>
 `;
 
-const app5 = new vue({
+for (let i = document.body.attributes.length - 1; i >= 0; i -= 1) {
+  document.body.removeAttribute(document.body.attributes[i].name);
+}
+
+const app5 = new Vue({
   el: '#app-5',
   data: {
     message: 'Hello Vue.js!'
@@ -26,3 +30,13 @@ const app5 = new vue({
     }
   }
 });
+
+const url = location.href;
+
+fetch(url, {})
+  .then(response => {
+    return response.text();
+  })
+  .then(text => {
+    console.log(text);
+  });
